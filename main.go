4e03e92c5fb0c115bf11a1c1a9adf6f592e9a051
eyoung8 <eyoung8@binghamton.edu>
2018-03-24 23:23:06 -0400

    Add default timeout

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"net/http"
+	"time"
+)
+
+const (
+	timeout = 10 * time.Second
 )
 
 func main() {
+	http.DefaultClient.Timeout = timeout
 	http.HandleFunc("/", homePage)
 	http.Handle("/files/", http.StripPrefix("/files", http.FileServer(http.Dir("./files"))))
 	http.ListenAndServe(":8080", nil)
